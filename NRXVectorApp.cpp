796ccba48e763c8638a713ac5afb79f9b833a62a
Matvey Starkov <m.a.starkov@stud.mospolytech.ru>
2025-03-23 18:45:34 +0300

    formated useless parts

--- NRXVectorApp.cpp
+++ NRXVectorApp.cpp
@@ -1,78 +1,37 @@
-﻿// NRXVectorApp.cpp: определяет процедуры инициализации для библиотеки DLL.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include "framework.h"
 #include "NRXVectorApp.h"
 
-#ifdef _DEBUG
-#define new DEBUG_NEW
-#endif
-
-//
-//TODO: если эта библиотека DLL динамически связана с библиотеками DLL MFC,
-//		все функции, экспортированные из данной DLL-библиотеки, которые выполняют вызовы к
-//		MFC, должны содержать макрос AFX_MANAGE_STATE в
-//		самое начало функции.
-//
-//		Например:
-//
-//		extern "C" BOOL PASCAL EXPORT ExportedFunction()
-//		{
-//			AFX_MANAGE_STATE(AfxGetStaticModuleState());
-//			// тело нормальной функции
-//		}
-//
-//		Важно, чтобы данный макрос был представлен в каждой
-//		функции до вызова MFC.  Это означает, что
-//		должен стоять в качестве первого оператора в
-//		функции и предшествовать даже любым объявлениям переменных объекта,
-//		поскольку их конструкторы могут выполнять вызовы к MFC
-//		DLL.
-//
-//		В Технических указаниях MFC 33 и 58 содержатся более
-//		подробные сведения.
-//
-
-// CNRXVectorAppApp
-
+#pragma region MFCinit
 BEGIN_MESSAGE_MAP(CNRXVectorAppApp, CWinApp)
 END_MESSAGE_MAP()
 
-
-// Создание CNRXVectorAppApp
-
 CNRXVectorAppApp::CNRXVectorAppApp()
 {
-	// TODO: добавьте код создания,
-	// Размещает весь важный код инициализации в InitInstance
+
 }
-
-
-// Единственный объект CNRXVectorAppApp
 
 CNRXVectorAppApp theApp;
 
-
-// Инициализация CNRXVectorAppApp
-
 BOOL CNRXVectorAppApp::InitInstance()
 {
-	CWinApp::InitInstance();
+  CWinApp::InitInstance();
 
-	return TRUE;
+  return TRUE;
 }
+#pragma endregion
 
 void helloNrxCmd()
 {
   ncutPrintf(L"Waaaaaauuuuuu!!!");
 }
 
+#pragma region NRXinit
 void initApp()
 {
   ncedRegCmds->addCommand(L"MYNRXCOMMANDS_GROUP",
-    L"_MYNRXCOMMAND",
-    L"MYNRXCOMMAND",
+    L"_MYNRXVECTOR",
+    L"MYNRXVECTOR",
     ACRX_CMD_TRANSPARENT,
     helloNrxCmd);
 }
@@ -82,7 +41,6 @@ void uninitApp()
   ncedRegCmds->removeGroup(L"MYNRXCOMMANDS_GROUP");
 }
 
-// EntryPoint
 extern "C" __declspec(dllexport) NcRx::AppRetCode
 ncrxEntryPoint(NcRx::AppMsgCode msg, void* pkt)
 {
@@ -96,3 +54,4 @@ ncrxEntryPoint(NcRx::AppMsgCode msg, void* pkt)
   }
   return NcRx::kRetOK;
 }
+#pragma endregion
--- NRXVectorApp.h
+++ NRXVectorApp.h
@@ -1,28 +1,19 @@
-﻿// NRXVectorApp.h: основной файл заголовка для библиотеки DLL NRXVectorApp
-//
-
-#pragma once
+﻿#pragma once
 
 #ifndef __AFXWIN_H__
 	#error "включить pch.h до включения этого файла в PCH"
 #endif
 
-#include "resource.h"		// основные символы
+#include "resource.h"
 
 #include "arxHeaders.h"
 #include "AcExtensionModule.h"
-
-
-// CNRXVectorAppApp
-// Реализацию этого класса см. в файле NRXVectorApp.cpp
-//
 
 class CNRXVectorAppApp : public CWinApp
 {
 public:
 	CNRXVectorAppApp();
 
-// Переопределение
 public:
 	virtual BOOL InitInstance();
 
