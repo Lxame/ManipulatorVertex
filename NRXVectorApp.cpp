9990d0a5779b8efa84d3f22ae9c119e06d3a19a3
Matvey Starkov <m.a.starkov@stud.mospolytech.ru>
2025-03-31 18:57:12 +0300

    made prototype

--- NRXVectorApp.cpp
+++ NRXVectorApp.cpp
@@ -21,6 +21,113 @@ BOOL CNRXVectorAppApp::InitInstance()
 }
 #pragma endregion
 
+void helloNrxCmd();
+void manipulatorVector();
+void addToModelSpace(NcDbObject &objId, NcDbEntity *pEnt);
+void getPolylineVertex();
+
+void manipulatorVector()
+{
+  nds_name ss, ent_name;
+  NRX::Int32 ssLen;
+  NcDbObjectId entId;
+  NcDbEntity* pEnt;
+  int ret = ncedSSGet(L"", NULL, NULL, NULL, ss);
+
+  if (ret != RTNORM)
+  {
+    ncutPrintf(L"\nОшибка выбора примитивов!\n");
+    return;
+  }
+
+  if (ncedSSLength(ss, &ssLen) != RTNORM)
+  {
+    ncutPrintf(L"\nОшибка получения количества примитивов!\n");
+    return;
+  }
+
+  for (NRX::Int32 i = 0; i < ssLen; ++i)
+  {
+    if (ncedSSName(ss, i, ent_name) != RTNORM)
+    {
+      ncutPrintf(L"\nОшибка получения имени приитива %d\n", i);
+      return;
+    }
+    if (ncdbGetObjectId(entId, ent_name) != Nano::eOk)
+    {
+      ncutPrintf(L"\nОшибка получения id приитива %d\n", i);
+      return;
+    }
+    if (ncdbOpenNcDbEntity(pEnt, entId, NcDb::kForWrite, true) == Nano::eOk)
+    {
+      ncutPrintf(L"\nПримитив открыт!\n");
+    }
+    else
+    {
+      ncutPrintf(L"\nОшибка открытия приитива %d\n", i);
+      return;
+    }
+  }
+
+  ncedSSFree(ss);
+}
+
+void getPolylineVertex()
+{
+  nds_name polyline;
+  nds_point pt;
+  if (ncedEntSel(L"Выберите полилинию", polyline, pt) != RTNORM)
+  {
+    ncutPrintf(L"\nОшибка выбора!\n");
+    return;
+  }
+
+  NcDbObjectId plineId;
+  NcDbEntity *pEnt;
+  if (ncdbGetObjectId(plineId, polyline))
+  {
+    ncutPrintf(L"\nНе удалось получить id объекта!\n");
+    return;
+  }
+
+  if (ncdbOpenNcDbEntity(pEnt, plineId, NcDb::kForRead) == Nano::eOk)
+  {
+    if (NcDb3dPolyline* pline = NcDb3dPolyline::cast(pEnt))
+    {
+      NcDbObjectIterator* iter = pline->vertexIterator();
+      NcDbObjectIdArray vertexArray;
+      for (iter->start(); !iter->done(); iter->step())
+      {
+        vertexArray.append(iter->objectId());
+        NcDb3dPolylineVertex* pVertex;
+        if (ncdbOpenNcDbObject((NcDbObject*&)pVertex, iter->objectId(), NcDb::kForRead) == Nano::eOk)
+        {
+          NcGePoint3d pos = pVertex->position();
+          ncutPrintf(L"\nКоординаты вершины: X=%f, Y=%f, Z=%f", pos.x, pos.y, pos.z);
+          pVertex->close();
+        }
+        else
+        {
+          ncutPrintf(L"\nНе удалось получить координаты вершины!");
+        }
+      }
+      delete iter;
+    }
+    else
+    {
+      ncutPrintf(L"\nВыбранный примитив не является прямой!\n");
+      pEnt->close();
+      return;
+    }
+  }
+  else
+  {
+    ncutPrintf(L"\nНе удалось открыть entity!\n");
+    return;
+  }
+
+}
+
 void helloNrxCmd()
 {
   ncutPrintf(L"Waaaaaauuuuuu!!!");
@@ -30,10 +137,20 @@ void helloNrxCmd()
 void initApp()
 {
   ncedRegCmds->addCommand(L"MYNRXCOMMANDS_GROUP",
+    L"_MYNRXTEST",
+    L"MYNRXRTEST",
+    ACRX_CMD_TRANSPARENT,
+    helloNrxCmd);
+  ncedRegCmds->addCommand(L"MYNRXCOMMANDS_GROUP",
     L"_MYNRXVECTOR",
     L"MYNRXVECTOR",
     ACRX_CMD_TRANSPARENT,
-    helloNrxCmd);
+    manipulatorVector);
+  ncedRegCmds->addCommand(L"MYNRXCOMMANDS_GROUP",
+    L"_MYNRXPLINE3DVERTEX",
+    L"MYNRXPLINE3DVERTEX",
+    ACRX_CMD_TRANSPARENT,
+    getPolylineVertex);
 }
 
 void uninitApp()
